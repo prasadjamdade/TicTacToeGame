9fcc17a1738668b076cc7e95bb034bf037354e71
prasadjamdade <prasadjamdade@hotmail.com>
2021-10-25 23:06:49 +0530

    Initiating project as cleared with all concepts with git.

--- src/com/workshop/ticTacToeGame/TicTacToeGameSimulator.java
+++ src/com/workshop/ticTacToeGame/TicTacToeGameSimulator.java
@@ -2,6 +2,6 @@ package com.workshop.ticTacToeGame;
 
 public class TicTacToeGameSimulator {
     public static void main(String[] args) {
-        System.out.println("\n\n Welcome to Tic_Tac_Toe game.");
+        System.out.println("\n\n Welcome to Tic_Tac_Toe game simulator.");
     }
 }
