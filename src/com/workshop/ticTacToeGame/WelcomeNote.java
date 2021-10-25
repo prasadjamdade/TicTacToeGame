78ee691cb1ff7742d520d79b5ca12f22704b428f
prasadjamdade <prasadjamdade@hotmail.com>
2021-10-25 19:27:36 +0530

    Initialization with welcome note.

---
+++ src/com/workshop/ticTacToeGame/WelcomeNote.java
@@ -0,0 +1,7 @@
+package com.workshop.ticTacToeGame;
+
+public class WelcomeNote {
+    public static void main(String[] args) {
+        System.out.println("\n\n Welcome to the Tic-Tac-Toe Game.");
+    }
+}
